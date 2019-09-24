3c01b87a6628fd9424e8034ca36d05f360bd01c7
rickynyairo <rickynyairo@gmail.com>
2019-09-24 20:05:23 +0300

    feat(unique): generate unique id  - adds functionality to generate unique id for a shopping cart  - adds tests to confirm functionality

--- src/controllers/__tests__/shoppingCart.controller.test.js
+++ src/controllers/__tests__/shoppingCart.controller.test.js
@@ -1,3 +1,59 @@
-test('for the truth', () => {
-  expect(1 + 1).toEqual(2);
+// eslint-disable-next-line import/no-extraneous-dependencies
+import '@babel/polyfill';
+import request from 'supertest';
+
+import app, { server } from '../..';
+import truncate from '../../test/helpers';
+
+describe('Shopping Cart Controller', () => {
+  // let shoppingCart;
+  // let shoppingCartId;
+  let accessToken;
+  beforeEach(async done => {
+    await truncate();
+
+    const customer = {
+      name: 'test customer',
+      password: 'password',
+      email: 'test_customer@email.com',
+    };
+    const response = await request(app)
+      .post('/customers/signup')
+      .set('Content-Type', 'application/json')
+      .send(customer);
+    // eslint-disable-next-line prefer-destructuring
+    accessToken = response.body.accessToken;
+    done();
+  });
+
+  afterAll(async done => {
+    server.close();
+    done();
+  });
+
+  describe('Generate Unique Id', () => {
+    it('should generate a unique shopping cart id for each authenticated user', done => {
+      request(app)
+        .get('/shoppingcart/generateUniqueId')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .end((error, res) => {
+          expect(res.status).toEqual(200);
+          expect(res.body).toHaveProperty('cart_id');
+          done();
+        });
+    });
+    it('should not generate a unique id for an unauthenticated user', done => {
+      request(app)
+        .get('/shoppingcart/generateUniqueId')
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer faketoken`)
+        .end((error, res) => {
+          expect(res.status).toEqual(401);
+          expect(res.body).toHaveProperty('error');
+          expect(res.body.error).toHaveProperty('message');
+          done();
+        });
+    });
+  });
 });
--- src/routes/api/shoppingCart.route.js
+++ src/routes/api/shoppingCart.route.js
@@ -1,25 +1,21 @@
 import { Router } from 'express';
 import ShoppingCartController from '../../controllers/shoppingCart.controller';
-
+import { validationMiddleware } from '../../middleware/validation';
+import { jwtAuthRequired } from '../../middleware/authentication';
 const router = Router();
-router.get('/shoppingcart/generateUniqueId', ShoppingCartController.generateUniqueCart);
+router.get(
+  '/shoppingcart/generateUniqueId',
+  jwtAuthRequired,
+  ShoppingCartController.generateUniqueCart
+);
 router.post('/shoppingcart/add', ShoppingCartController.addItemToCart);
 router.get('/shoppingcart/:cart_id', ShoppingCartController.getCart);
 router.put('/shoppingcart/update/:item_id', ShoppingCartController.updateCartItem);
 router.delete('/shoppingcart/empty/:cart_id', ShoppingCartController.emptyCart);
 router.delete('/shoppingcart/removeProduct/:item_id', ShoppingCartController.removeItemFromCart);
 router.post('/orders', ShoppingCartController.createOrder);
-router.get(
-  '/orders/inCustomer',
-  ShoppingCartController.getCustomerOrders
-);
-router.get(
-  '/orders/:order_id',
-  ShoppingCartController.getOrderSummary
-);
-router.post(
-  '/stripe/charge',
-  ShoppingCartController.processStripePayment
-);
+router.get('/orders/inCustomer', ShoppingCartController.getCustomerOrders);
+router.get('/orders/:order_id', ShoppingCartController.getOrderSummary);
+router.post('/stripe/charge', ShoppingCartController.processStripePayment);
 
 export default router;
