6e1b4de67b8fa0d4f635499bb6089255ec01dce3
Ricky <rickynyairo@gmail.com>
2019-09-26 15:33:40 +0300

    feat(shipping): get shipping regions and shipping types (#8)
    - adds tests for shippin

--- src/routes/api/shipping.route.js
+++ src/routes/api/shipping.route.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import ShippingController from '../../controllers/shipping.controller';
+import { jwtAuthRequired } from '../../middleware/authentication';
 
 const router = Router();
-
+router.use('/shipping', jwtAuthRequired);
 router.get('/shipping/regions', ShippingController.getShippingRegions);
 router.get('/shipping/regions/:shipping_region_id', ShippingController.getShippingType);
 
